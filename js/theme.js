372e4a4f5a79460ae55fdc1f79943d03bdf77a09
harunbiswas <77938106+harunbiswas@users.noreply.github.com>
2021-02-17 03:18:48 +0600

    first commit

---
+++ js/theme.js
@@ -0,0 +1,15 @@
+$(document).ready(() => {
+
+    $('.image-isotope').isotope({
+        itemSelector: '.image-item',
+        percentPosition: true,
+        masonry: {
+          // use outer width of grid-sizer for columnWidth
+          columnWidth: 2
+        }
+      })
+
+
+
+
+})
